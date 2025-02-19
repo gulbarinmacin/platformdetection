6ffdbae479eeba82e12c1cede7d71e3a2bdf9feb
Gülbarin Maçin <85343560+gulbarinmacin@users.noreply.github.com>
2025-02-19 23:26:31 +0300

    Update detector.py

--- detector.py
+++ detector.py
@@ -1,75 +1,50 @@
 import requests
 import pandas as pd
 from concurrent.futures import ThreadPoolExecutor, as_completed
+from tqdm import tqdm
 
-def check_site(url):
-    if not url.startswith('http'):
-        url = 'https://' + url
 
-    try:
-        response = requests.get(url, timeout=15)
-        response.raise_for_status()
-        return True, url
-    except requests.exceptions.RequestException as e:
-        return False, str(e)  # Hataları döndür
-
-def find_platform(url):
-    platforms = {  # you can customize these platforms according to your preferences
-        "Shopify": "shopify",
+def check_and_find_platform(row):
+    platforms = {
         "WooCommerce": "woocommerce",
-        "Ticimax": "ticimax",
-        "Wix" : "wix",
-        "OpenCart": "route=product",
-        "Magento": "mage-",
-        "PrestaShop": "prestashop",
-        "BigCommerce": "bigcommerce"
+        "CS Cart": "cscart.com",
+        "Summer Cart": ".summercart.",
+        "Jumpseller": "jumpseller.com",
+        "Hyva Themes": "hyva.setCookie",
+        "X-Cart": "content=\"X-Cart\"",
+        "Bilginet": "bilginet.com",
+        "Yampi": "yampi.io",
+        "Sellix": "sellix.io"
     }
 
-    if not url.startswith('http'):
-        url = 'https://' + url
+    website = row['E-commerce Site']
+    if not website.startswith('https'):
+        website = 'https://' + e_ticaret_sitesi
 
     try:
-        response = requests.get(url, timeout=15)
+        response = requests.get(website, timeout=15)
         response.raise_for_status()
         html_code = response.text.lower()
 
         for platform, keyword in platforms.items():
             if keyword.lower() in html_code:
-                return platform, "OK"
-        return "Unknown Platform", "OK"
+                return [row['Company Name'], website, platform, "OK"]
+        return [row['Company Name'], e_ticaret_sitesi, "Unknown Platform", "OK"]
 
     except requests.exceptions.RequestException as e:
-        return None, str(e) 
+        return [row['Company Name'], e_ticaret_sitesi, "Connection Error", str(e)]
 
-df = pd.read_excel('/content/sampledata.xlsx') # data path
+df = pd.read_excel('you_data_path')
 
-def process_site(row, processed_urls):
-    e_ticaret_sitesi = row['E Ticaret Sitesi']
-    if e_ticaret_sitesi in processed_urls:
-        return None  
-
-    is_valid, message = check_site(e_ticaret_sitesi)
-    processed_urls.add(e_ticaret_sitesi) 
-
-    if is_valid:
-        platform, _ = find_platform(e_ticaret_sitesi)
-        return [row['İşletme Unvanı'], e_ticaret_sitesi, platform]
-    else:
-        print(f"{e_ticaret_sitesi}: {message}") 
-    return None
-
-valid_sites = []
-processed_urls = set() 
-
+results = []
 with ThreadPoolExecutor(max_workers=10) as executor:
-    futures = {executor.submit(process_site, row, processed_urls): row for index, row in df.iterrows()}
-    for future in as_completed(futures):
+    futures = {executor.submit(check_and_find_platform, row): row for index, row in df.iterrows()}
+    for future in tqdm(as_completed(futures), total=len(df), desc="Processing"):
         result = future.result()
-        if result:
-            valid_sites.append(result)
+        results.append(result)
 
-platforms_df = pd.DataFrame(valid_sites, columns=['İşletme Unvanı', 'E Ticaret Sitesi', 'Platform'])
+platforms_df = pd.DataFrame(results, columns=['Company Name', 'E-comerce Site', 'Platform', 'Status'])
 
-platforms_df.to_excel('platforms.xlsx', index=False)
+platforms_df.to_excel('add_name_for_output', index=False)
 
-print("Operation complete. Check platforms.xlsx for platforms.")
+print("Operation complete. Check document for platforms.")
